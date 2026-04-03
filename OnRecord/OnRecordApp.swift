8e32ac3194fae1feb44aa91fd5afcbcc24ebb320
gomez1112 <54489633+gomez1112@users.noreply.github.com>
2026-04-03 09:43:44 -0400

    Initial Commit

---
+++ OnRecord/OnRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnRecordApp.swift
+//  OnRecord
+//
+//  Created by Gerard Gomez on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct OnRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
